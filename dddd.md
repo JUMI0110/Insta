2a9576a088624e68f9c875fd30c61d783528cdff
jumi <ajsel422@gmail.com>
2024-08-08 10:09:41 +0900

    media 관련 settings 설정

--- dddd.md
+++ dddd.md
@@ -1,2 +1,10 @@
+게시물 기능 -> 로그인 기능
 Model
 ImageField() -> Pillow 라이브러리 필요
+upload_to=업로드할 파일
+MEDIA_ROOT/uploads  설정하고싶은 url
+# 업로드한 사진을 저장할 위치 (실제 폴더 경로)
+MEDIA_ROOT = BASE_DIR / 'media'
+~/Desktop/DMF/insta/media
+# media url 인터넷을 통해 사진을 보기위해 접근할 수 있는 경로설정
+MEDIA_URL = '/media/'
--- insta/settings.py
+++ insta/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 업로드한 사진을 저장할 위치 (실제 폴더 경로)
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# media url 인터넷을 통해 사진을 보기위해 접근할 수 있는 경로설정
+# 미디어 경로를 처리할 url
+MEDIA_URL = '/media/'
